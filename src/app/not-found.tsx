e164bbdaa7a13536c393b100decc2698ddefa868
Eng.Russi <m.hassan@sharidah.sa>
2025-08-20 10:12:45 +0300

    not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+import { h1 } from "framer-motion/client";
+
+
+export default function NotFound(){
+    return <section className="py-28 container max-w-7xl mx-auto px-4">
+        <h1 className="text-4xl md:text-4xl font-bold mb-6 text-center">This Not Defined</h1>
+    </section>
+}
