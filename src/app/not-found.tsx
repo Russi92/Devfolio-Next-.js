eabda51e3efd589f9607e94f036176b03d0343fd
Eng.Russi <m.hassan@sharidah.sa>
2025-08-21 17:36:13 +0300

    not found page finshed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,30 @@
+// app/not-found.tsx
+import Image from "next/image";
+import Link from "next/link";
 
+export default function NotFound() {
+  return (
+    <section className="relative w-full h-screen flex items-center justify-center text-center">
+      {/* Background Image */}
+      <Image
+        src="/404.jpg"
+        alt="404 Not Found"
+        fill
+        priority
+        className="object-contain"
+      />
 
-
-export default function NotFound(){
-    return <section className="py-28 container max-w-7xl mx-auto px-4">
-        <h1 className="text-4xl md:text-4xl font-bold mb-6 text-center">This Not Defined</h1>
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/60 flex flex-col items-center justify-center px-4">
+   
+        <Link
+          href="/"
+          className="bg-primary text-white px-6 py-3 rounded-lg shadow-lg 
+                     hover:bg-primary/80 transition text-base md:text-lg"
+        >
+          Back to Home
+        </Link>
+      </div>
     </section>
+  );
 }
